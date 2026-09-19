404c424f122e4b594a867ce95694626d0c4dd9c8
basic-app <dev@basic-app.com>
2026-09-19 09:40:40 +0300

    n/a

---
+++ src/Migration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Basic App Dev Team
+ * @license MIT
+ */
+namespace BasicApp\Core;
+
+use CodeIgniter\Database\Migration as BaseMigration;
+
+abstract class Migration extends BaseMigration
+{
+}
